bd7b9070765cf694c2840a2ca5fa1305fc14174a
Samuel James <bsbcraftplays@gmail.com>
2021-11-09 12:18:58 -0300

    Corrigindo ULA

--- riscv_multiciclo/RV32_ALU.vhd
+++ riscv_multiciclo/RV32_ALU.vhd
@@ -20,89 +20,91 @@ end RV32_ALU;
 architecture RV32_ALU_ARCH of RV32_ALU is
     constant TRUE_32 : std_logic_vector(31 downto 0) := x"00000001";
     constant FALSE_32 : std_logic_vector(31 downto 0) := x"00000000";
+    signal   my_result : std_logic_vector(31 downto 0);
     begin
         process(opcode, A, B)
         begin    
             -- ADD A, B     ALU_Result recebe a soma das entradas A, B
             if opcode = "0000" then
-                ALU_Result <= std_logic_vector(resize(unsigned(A) + unsigned(B), 32));
+                my_result <= std_logic_vector(resize(unsigned(A) + unsigned(B), 32));
             end if;
 
             -- SUB A, B     ALU_Result recebe A - B
             if opcode = "0001" then 
-                ALU_Result <= std_logic_vector(resize(unsigned(A) - unsigned(B), 32));
+                my_result <= std_logic_vector(resize(unsigned(A) - unsigned(B), 32));
             end if;
 
             -- AND A, B     ALU_Result recebe a operação lógica A and B, bit a bit
             if opcode = "0010" then
-                ALU_Result <= A AND B;
+                my_result <= A AND B;
             end if;
             -- OR A, B      ALU_Result recebe a operação lógica A or B, bit a bit
             if opcode = "0011" then
-                ALU_Result <= A OR  B;
+                my_result <= A OR  B;
             end if;
 
             -- XOR A, B     ALU_Result recebe a operação lógica A xor B, bit a bit
             if opcode = "0100" then
-                ALU_Result <= A XOR B; 
+                my_result <= A XOR B; 
             end if;
 
             -- SLL A, B        ALU_Result recebe a entrada A deslocada B bits à esquerda
             if opcode = "0101" then
-                ALU_Result <= resize(shift_left(unsigned(A), unsigned(B)), 32);
+                my_result <= std_logic_vector(resize(unsigned(A) sll to_integer(unsigned(B)), 32));
             end if; 
 
             -- SRL A, B        ALU_Result recebe a entrada A deslocada B bits à direita sem sinal
             if opcode = "0110" then
-                ALU_Result <= resize(shift_right(unsigned(A), unsigned(B)), 32);
-            end if;  
+                my_result <= std_logic_vector(resize(unsigned(A) srl to_integer(unsigned(B)), 32));
+            end if;
 
             -- SRA A, B        ALU_Result recebe a entrada A deslocada B bits à direita com sinal 
             if opcode = "0111" then
-                ALU_Result <= resize(shift_right(signed(A), unsigned(B)), 32);
+                my_result <= std_logic_vector(resize(unsigned(A) sra natural(to_integer(unsigned(B))), 32));
             end if;  
 
             -- SLT A, B        ALU_Result = 1 se A < B, com sinal
             if opcode = "1000" then
-                ALU_Result <= TRUE_32 when (signed(A) < signed(B)) else FALSE_32;
+                my_result <= TRUE_32 when (signed(A) < signed(B)) else FALSE_32;
             end if;  
 
             -- SLTU A, B       ALU_Result = 1 se A < B, sem sinal
             if opcode = "1000" then
-                ALU_Result <= TRUE_32 when (unsigned(A) < unsigned(B)) else FALSE_32;
+                my_result <= TRUE_32 when (unsigned(A) < unsigned(B)) else FALSE_32;
             end if;  
 
             -- SGE A, B        ALU_Result = 1 se A ≥ B, com sinal
             if opcode = "1010" then
-                ALU_Result <= TRUE_32 when (signed(A) >= signed(B)) else FALSE_32;
+                my_result <= TRUE_32 when (signed(A) >= signed(B)) else FALSE_32;
             end if;  
 
             -- SGEU A, B       ALU_Result = 1 se A ≥ B, sem sinal 
             if opcode = "1011" then
-                ALU_Result <= TRUE_32 when (unsigned(A) >= unsigned(B)) else FALSE_32;
+                my_result <= TRUE_32 when (unsigned(A) >= unsigned(B)) else FALSE_32;
             end if;  
 
             -- SEQ A, B        ALU_Result = 1 se A == B
             if opcode = "1100" then
-                ALU_Result <= TRUE_32 when (A = B) else FALSE_32;
+                my_result <= TRUE_32 when (A = B) else FALSE_32;
             end if; 
 
             -- SNE A, B        ALU_Result = 1 se A != B
             if opcode = "1101" then
-                ALU_Result <= FALSE_32 when (A = B) else TRUE_32;
+                my_result <= FALSE_32 when (A = B) else TRUE_32;
             end if;
 
             if opcode = "1110" or opcode = "1111" then
-                ALU_Result <= FALSE_32;
+                my_result <= FALSE_32;
             end if;
                     
         end process;
 
-        process(ALU_result)
+        process(my_result)
         begin
-            with ALU_Result select
-            Zero <= '0' when x"00000000",
-                    '1' when others;
+            with my_result select
+                Zero <= '0' when x"00000000",
+                        '1' when others;
+            ALU_result <= my_result;
         end process;
 
 end RV32_ALU_ARCH;
